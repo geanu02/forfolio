218f772e659dd0bf8fd0aa7994fcbb01effd23ed
Gian Carlo Torres <geanu@outlook.com>
2022-08-16 09:23:11 -0400

    first commit

---
+++ src/Components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div class="container-hero">
+        <h1>Joaquin Carlos Urbiztondo de Jesus</h1>
+        <h4 class="subtitle-hero">Multi-awarded Writer</h4>
+    </div>
+  )
+}
+
+export default Hero
